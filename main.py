70df83aedafd96d786dc6b00342db464cf6d49f0
XROll-71 <122893313+XROll-71@users.noreply.github.com>
2024-04-21 20:17:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import requests
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 import random
+
+# Запускаем логгирование
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
