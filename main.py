2113d2b0521d0b02032de62060176b4dfb44e9a7
DudukaAl <151047111+DudukaAl@users.noreply.github.com>
2024-04-21 20:31:15 +0300

    Update main.py
    добавил функцию /start

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ import random
 # Запускаем логгирование
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Функция-обработчик команды /start
+async def start(update, context):
+    user_name = update.message.from_user.first_name
+    await update.message.reply_text(
+        f"Привет, {user_name}! Я бот от DDCompany. Рад вас видеть!",
+        reply_markup=get_main_keyboard_markup()
+    )
